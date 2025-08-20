784e5bcda936607242f34a6da3ba848b60ecacfa
abi780 <abinayan@nmsworks.co.in>
2025-08-20 13:07:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    version = os.environ.get("VERSION", "v2")
+    return f"Hello from Flask Demo-4 Version: {version}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
